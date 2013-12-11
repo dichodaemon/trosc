d1455314c93f86cd94ffb104a05ef105deb6814b
Dizan Vasquez <dix_ans@fastmail.fm>
2013-12-11 15:50:45 +0100

    Initial commit

---
+++ src/car1/python/cars/car1.py
@@ -0,0 +1,49 @@
+import numpy as np
+from math import *
+import sys
+
+def steering( track_yaw, track_curvature, track_distance, track_width ):
+  yaw_error = track_yaw 
+  if track_curvature > 0.0:
+    track_distance -= 3.0
+  else:
+    track_distance += 3.0
+  lane_error = atan2( track_distance, 20 )
+  return -yaw_error - lane_error
+
+def max_speed( curvature, track_yaw, track_distance, mu = 0.3 ):
+  result = 0
+  if curvature == 0:
+    result = 20
+  else:
+    result = ( mu * 9.81 / np.abs( curvature ) ) ** 0.5
+  return result
+
+def acceleration( 
+  speed, desired_speed, 
+  gear_ratio, max_rpm, wheel_radius,
+  acceleration_margin = 1.0
+) :
+  if desired_speed > speed + acceleration_margin:
+    return 1.0
+  else:
+    return desired_speed / wheel_radius * gear_ratio / max_rpm
+
+def gear( 
+  speed, 
+  gear, gear_ratio, lower_gear_ratio, max_rpm, wheel_radius, 
+  shift = 0.9, shift_margin = 4.0 
+):
+  if gear <= 0:
+    return 1
+  omega = max_rpm / gear_ratio
+  if omega * wheel_radius * shift < speed:
+    return gear + 1
+  if lower_gear_ratio != 0.0:
+    omega = max_rpm / lower_gear_ratio
+    if gear > 1.0 and omega * wheel_radius * shift > speed + shift_margin:
+      return gear - 1
+  return gear
+
+def brake():
+  return 0.0
---
+++ src/trosc/python/trosc/facade.py
@@ -0,0 +1,68 @@
+import sys
+import ctypes
+import sysv_ipc as ipc
+
+class Command( ctypes.Structure ):
+  _fields_ = [ ( "steering", ctypes.c_float ), 
+               ( "acceleration", ctypes.c_float ),
+               ( "brake", ctypes.c_float ),
+               ( "gear", ctypes.c_int ) ]
+
+class Status( ctypes.Structure ):
+  _fields_ = [ ( "rpm", ctypes.c_float ),
+               ( "gear", ctypes.c_int ),
+               ( "gear_ratio", ctypes.c_float ),
+               ( "lower_gear_ratio", ctypes.c_float ),
+               ( "max_rpm", ctypes.c_float ),
+               ( "wheel_radius", ctypes.c_float ),
+               ( "track_yaw", ctypes.c_float ),
+               ( "track_distance", ctypes.c_float ),
+               ( "track_curvature", ctypes.c_float ),
+               ( "track_width", ctypes.c_float ),
+               ( "speed", ctypes.c_float ),
+               ( "yaw", ctypes.c_float ),
+               ( "x", ctypes.c_float ),
+               ( "y", ctypes.c_float )]
+
+class Obstacle( ctypes.Structure ):
+  _fields_ = [ ( "id", ctypes.c_byte ),
+               ( "x", ctypes.c_float ),
+               ( "y", ctypes.c_float ),
+               ( "vX", ctypes.c_float ),
+               ( "vY", ctypes.c_float ) ]
+
+class Buffer( ctypes.Structure ):
+  _fields_ = [ ( "command", Command ), 
+               ( "status", Status ),
+               ( "n_obstacles", ctypes.c_int ),
+               ( "obstacles", Obstacle * 100 ) ]
+
+class Facade( object ):
+  def __init__( self, key = 5555 ):
+    libc = ctypes.CDLL( "libc.so.6" )
+    libc.shmat.restype = ctypes.POINTER( Buffer )
+
+    segment_id = libc.shmget( key, 0, 0 )
+    self.buffer = libc.shmat( segment_id, 0, 0 )
+    self.s = ipc.Semaphore( key )
+  
+  def __get_status( self ):
+    self.s.acquire()
+    b = Buffer.from_buffer_copy( self.buffer.contents )
+    self.s.release()
+    return b.status
+
+  def __get_obstacles( self ):
+    self.s.acquire()
+    b = Buffer.from_buffer_copy( self.buffer.contents )
+    self.s.release()
+    return b.n_obstacles, b.obstacles
+
+  def __set_command( self, command ):
+    self.s.acquire()
+    self.buffer.contents.command = command
+    self.s.release()
+
+  status = property( __get_status )
+  obstacles = property( __get_obstacles )
+  command = property( None, __set_command )
