f19777d647ea0c8bff07a1ab833672fa9d95fee9
Dizan Vasquez <dix_ans@fastmail.fm>
2013-12-13 16:36:56 +0100

    First version using braking distance

--- src/car1/python/cars/car1.py
+++ src/car1/python/cars/car1.py
@@ -3,8 +3,7 @@ from math import *
 import sys
 
 def steering( 
-  track_yaw, track_curvature, track_distance, track_width, 
-  next_curvature, next_distance
+  track_yaw, track_curvature, track_distance, track_width 
 ):
   yaw_error = track_yaw 
   #if next_curvature > 0.0:
@@ -19,6 +18,12 @@ def steering(
   lane_error = atan2( track_distance, 20 )
   return -yaw_error - lane_error
 
+def braking_distance( speed, desired_speed, mu = 0.3 ):
+  if speed < desired_speed:
+    return 1E6
+  else:
+    return ( speed ** 2 - desired_speed ** 2 ) / ( 2.0 * mu * 9.81 ) + 2.0
+
 def max_speed( curvature, mu = 0.3 ):
   result = 0
   if curvature == 0:
@@ -29,18 +34,28 @@ def max_speed( curvature, mu = 0.3 ):
 
 def acceleration( 
   speed, desired_speed, 
-  gear_ratio, max_rpm, wheel_radius,
-  acceleration_margin = 1.0
+  gear_ratio, max_rpm, wheel_radius, rpm
 ) :
-  if desired_speed > speed + acceleration_margin:
+  if desired_speed < speed:
+    sys.stderr.write( "zero acceleration\n" )
+    return 0.0
+
+  if speed + 10 < desired_speed:
+    sys.stderr.write( "max acceleration\n" )
     return 1.0
+    #wheel_diameter = wheel_radius * 2 * pi
+    #desired_rpm = ( desired_speed / wheel_diameter ) * gear_ratio
   else:
-    return desired_speed / wheel_radius * gear_ratio / max_rpm
+    desired_rpm = rpm * speed / desired_speed
+  
+  result = min( desired_rpm / max_rpm, 1.0 )
+  sys.stderr.write( "acceleration: %f, speed:%f, desired_speed: %f, gear ratio: %f, desired_rpm: %f, max_rpm: %f, rpm: %f\n" % ( result, speed, desired_speed, gear_ratio, desired_rpm, max_rpm, rpm ) ) 
+  return result
 
 def gear( 
-  speed, 
-  gear, gear_ratio, lower_gear_ratio, max_rpm, wheel_radius, 
-  shift = 0.9, shift_margin = 4.0 
+speed, 
+gear, gear_ratio, lower_gear_ratio, max_rpm, wheel_radius, 
+shift = 0.8, shift_margin = 4.0 
 ):
   if gear <= 0:
     return 1
